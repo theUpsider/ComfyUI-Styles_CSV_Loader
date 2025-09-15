292be57ab148eff7a8b106413ed6fa2501b357ac
David Fischer <davidvfischer@gmail.com>
2025-09-15 17:17:53 +0200

    Reorder import statement for styles_csv_loader in __init__.py

--- __init__.py
+++ __init__.py
@@ -1,6 +1,6 @@
-from .styles_csv_loader import *
 import sys
 import os
 current_dir = os.path.dirname(os.path.abspath(__file__))
 if current_dir not in sys.path:
     sys.path.insert(0, current_dir)
+from .styles_csv_loader import *
