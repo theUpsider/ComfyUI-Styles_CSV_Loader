d479c5a6670481a649782e44b4e6f0975faa9d98
David Fischer <davidvfischer@gmail.com>
2025-09-15 17:17:36 +0200

    Refactor import statements and update sys.path handling in __init__.py

--- __init__.py
+++ __init__.py
@@ -1,1 +1,6 @@
-from styles_csv_loader import StylesCSVLoader
+from .styles_csv_loader import *
+import sys
+import os
+current_dir = os.path.dirname(os.path.abspath(__file__))
+if current_dir not in sys.path:
+    sys.path.insert(0, current_dir)
